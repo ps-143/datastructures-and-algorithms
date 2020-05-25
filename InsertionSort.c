7ca79960d447b5b39146205d9462dd4e90dd98e5
Prateek Sharma <ps.prateek.sharma143@gmail.com>
2020-05-25 16:25:26 +0530

    Corrected INsertionSort

--- InsertionSort.c
+++ InsertionSort.c
@@ -4,13 +4,12 @@ void InsertionSort(int A[], int n) {
 	int j = 1;
 	while(j < n) {
 		int i = j - 1;
-		int k;
-		while(A[i+1] < A[i] && i >= 0) {
-			k = A[i+1];
+		int key = A[j];
+		while(key < A[i] && i >= 0) {
 			A[i+1] = A[i];
-			A[i] = k;
 			i--;
 		}
+		A[i+1] = key;
 		j++;
 	}
 }
